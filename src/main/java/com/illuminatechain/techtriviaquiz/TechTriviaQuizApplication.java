3729849a86f607fdb69e926a7bf19a9a301269bc
이승환 <seaung13@gmail.com>
2024-11-04 07:12:18 +0900

    Initial commit

---
+++ src/main/java/com/illuminatechain/techtriviaquiz/TechTriviaQuizApplication.java
@@ -0,0 +1,13 @@
+package com.illuminatechain.techtriviaquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTriviaQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTriviaQuizApplication.class, args);
+    }
+
+}
